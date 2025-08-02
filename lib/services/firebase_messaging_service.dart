f2bec7e10f6b315ca2d2aa57545b2c09ff835407
pasindujayakoi77 <pasindujaya687@gmail.com>
2025-08-03 01:09:43 +0530

    Add user topic subscription in FCMService; subscribe users to their tags for notifications✏️

--- lib/services/firebase_messaging_service.dart
+++ lib/services/firebase_messaging_service.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
@@ -9,6 +11,20 @@ class FCMService {
 
   static Future<void> init() async {
     await _fcm.requestPermission();
+
+    // Subscribe to role-based or default topics
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      final data = doc.data();
+      final tags = List<String>.from(data?['tags'] ?? []);
+      for (final tag in tags) {
+        await _fcm.subscribeToTopic(tag);
+      }
+    }
 
     const androidSettings = AndroidInitializationSettings(
       '@mipmap/ic_launcher',
