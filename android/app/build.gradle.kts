4df558a8b1eb2ba12319953402d276785784161e
pasindujayakoi77 <pasindujaya687@gmail.com>
2025-08-02 17:34:06 +0530

    Fix Android build configuration: set minSdk to 23 and restore ndkVersion✏️

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
 android {
     namespace = "com.example.ceylon"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -26,7 +25,7 @@ android {
         applicationId = "com.example.ceylon"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -39,6 +38,8 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    ndkVersion = "27.0.12077973"
 }
 
 flutter {
